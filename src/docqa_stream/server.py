ea596e07593c3bdad1fbdac9fcf4463d354c21ec
ma2za <mazzapaolo2019@gmail.com>
2023-08-24 15:25:54 +0200

    🦍 stream endpoint done

--- src/docqa_stream/server.py
+++ src/docqa_stream/server.py
@@ -6,11 +6,14 @@ import uvicorn
 import weaviate
 from dotenv import load_dotenv
 from fastapi import FastAPI, UploadFile
-from langchain.chains import RetrievalQA
+from langchain import LLMChain
+from langchain.chains import RetrievalQA, StuffDocumentsChain
+from langchain.chat_models import AzureChatOpenAI
+from langchain.prompts import PromptTemplate, HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.schema import SystemMessage, HumanMessage
 from langchain.vectorstores import Weaviate
+from starlette.responses import StreamingResponse
 from unstructured.partition.pdf import partition_pdf
-
-from src.docqa_stream.utils.llm import get_model
 
 load_dotenv()
 
@@ -19,7 +22,6 @@ app = FastAPI()
 client = weaviate.Client(os.environ["WEAVIATE_URL"])
 vectorstore = Weaviate(client=client, index_name="Document", text_key="content")
 logger = logging.getLogger(__name__)
-llm = get_model()
 
 
 @app.get("/")
@@ -27,10 +29,43 @@ async def root():
     return {"message": "Hello World"}
 
 
+def openai_streamer(retrieval_qa: RetrievalQA, query: str):
+    for resp in retrieval_qa.run(query):
+        yield resp
+
+
 @app.get("/query")
 async def query(question: str):
-    qa_chain = RetrievalQA.from_chain_type(llm, retriever=vectorstore.as_retriever())
-    return qa_chain({"query": question})
+    llm = AzureChatOpenAI(deployment_name="finetuner", streaming=True)
+
+    messages = [
+        SystemMessage(
+            content=(
+                "You are a world class algorithm to answer "
+                "questions in a specific format."
+            )
+        ),
+        HumanMessage(content="Answer question using the following context"),
+        HumanMessagePromptTemplate.from_template("{context}"),
+        HumanMessagePromptTemplate.from_template("Question: {question}"),
+        HumanMessage(content="Tips: Make sure to answer in the correct format"),
+    ]
+    prompt = ChatPromptTemplate(messages=messages)
+
+    qa_chain = LLMChain(llm=llm, prompt=prompt)
+    doc_prompt = PromptTemplate(
+        template="Content: {page_content}",
+        input_variables=["page_content"],
+    )
+    final_qa_chain = StuffDocumentsChain(
+        llm_chain=qa_chain,
+        document_variable_name="context",
+        document_prompt=doc_prompt,
+    )
+    retrieval_qa = RetrievalQA(
+        retriever=vectorstore.as_retriever(), combine_documents_chain=final_qa_chain
+    )
+    return StreamingResponse(openai_streamer(retrieval_qa, question), media_type='text/event-stream')
 
 
 @app.post("/upload")
@@ -38,28 +73,8 @@ async def create_upload_file(file: UploadFile):
     data = await file.read()
     elements = partition_pdf(file=io.BytesIO(data))
     text = [ele.text for ele in elements]
-    vectorstore.add_texts(["\n".join(text)])
-    # data_object = {
-    #     'title': file.filename,
-    #     'content': "\n".join(text),
-    # }
-    #
-    # uuid = generate_uuid5(data_object)
-    # existing_data_object = client.data_object.get_by_id(
-    #     uuid,
-    #     class_name='Document',
-    # )
-    # if existing_data_object is not None:
-    #     return existing_data_object
-    #
-    # uuid = client.data_object.create(
-    #     data_object=data_object,
-    #     class_name='Document',
-    #     uuid=uuid,
-    # )
-    out = vectorstore.search("paolo", "similarity")
-
-    return {"out": out}
+    response = vectorstore.add_texts(["\n".join(text)])
+    return {"response": response}
 
 
 if __name__ == "__main__":
